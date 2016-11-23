5681aedfdffe7b6040e0cfecd3f4c43d32fd9f94
donggeon <dkkim04@naver.com>
2016-11-23 18:47:36 +0900

    로그인 성공 후 액티비티(테스트용)

---
+++ Yamyam/app/src/main/java/dongguk/yamyam/activity/AfterLoginActivity.java
@@ -0,0 +1,89 @@
+package dongguk.yamyam.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import dongguk.yamyam.R;
+import dongguk.yamyam.helper.SQLiteHandler;
+import dongguk.yamyam.helper.SessionManager;
+
+/**
+ * Created by DK on 2016-10-30.
+ */
+public class AfterLoginActivity extends Activity {
+
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_login);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // continueApp();
+                logoutUser();
+            }
+        });
+    }
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+
+        Intent intent = new Intent(AfterLoginActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void continueApp() {
+        Intent intent = new Intent(AfterLoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
