dec7bc3125e13b8cc9e0a58f3a4ee1f2ec74508c
하나 <khn_0412@naver.com>
2016-11-23 19:37:51 +0900

    지역을 선택하는 프레그먼트 activity_region레이아웃을 출력하고 각 버튼에 대한 리스너 구현

---
+++ Yamyam/app/src/main/java/com/example/Yamyam/RegionFragment.java
@@ -0,0 +1,332 @@
+package com.example.Yamyam;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class RegionFragment extends Fragment {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.activity_region,container, false);
+
+        Button button_1=(Button)view.findViewById(R.id.button_0);
+        button_1.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_2=(Button)view.findViewById(R.id.button1);
+        button_2.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_3=(Button)view.findViewById(R.id.button2);
+        button_3.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }); Button button_4=(Button)view.findViewById(R.id.button3);
+        button_4.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_5=(Button)view.findViewById(R.id.button4);
+        button_5.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_6=(Button)view.findViewById(R.id.button5);
+        button_6.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_7=(Button)view.findViewById(R.id.button6);
+        button_7.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_8=(Button)view.findViewById(R.id.button7);
+        button_8.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }); Button button_9=(Button)view.findViewById(R.id.button8);
+        button_9.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_10=(Button)view.findViewById(R.id.button9);
+        button_10.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_11=(Button)view.findViewById(R.id.button10);
+        button_11.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_12=(Button)view.findViewById(R.id.button11);
+        button_12.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_13=(Button)view.findViewById(R.id.button12);
+        button_13.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_14=(Button)view.findViewById(R.id.button13);
+        button_14.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_15=(Button)view.findViewById(R.id.button14);
+        button_15.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_16=(Button)view.findViewById(R.id.button15);
+        button_16.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_17=(Button)view.findViewById(R.id.button16);
+        button_17.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_18=(Button)view.findViewById(R.id.button17);
+        button_18.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_19=(Button)view.findViewById(R.id.button18);
+        button_19.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_20=(Button)view.findViewById(R.id.button19);
+        button_20.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_21=(Button)view.findViewById(R.id.button20);
+        button_21.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_22=(Button)view.findViewById(R.id.button21);
+        button_22.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_23=(Button)view.findViewById(R.id.button22);
+        button_23.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_24=(Button)view.findViewById(R.id.button23);
+        button_24.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_25=(Button)view.findViewById(R.id.button24);
+        button_25.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        Button button_26=(Button)view.findViewById(R.id.button25);
+        button_26.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent i = new Intent(getActivity(), DetailActivity.class);
+                    startActivity(i);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        return view;
+    }
+}
