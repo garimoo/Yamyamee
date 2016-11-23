ee8f63d76b396f12319fd44fd909befdf00673a6
하나 <khn_0412@naver.com>
2016-11-23 19:37:17 +0900

    탭레이아웃에서 두번째 프레그먼트(검색 화면) 출력을 위한 코드 임의로 다른 레이아웃을 출력함

---
+++ Yamyam/app/src/main/java/com/example/Yamyam/SearchFragment.java
@@ -0,0 +1,18 @@
+package com.example.Yamyam;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+         View view=inflater.inflate(R.layout.content_nav_drawer,container, false);
+
+        return view;
+    }
+}
