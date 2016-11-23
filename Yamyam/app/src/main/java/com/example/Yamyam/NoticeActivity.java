9ca59b27629910093a68bbd9357e50fee9b7079b
하나 <khn_0412@naver.com>
2016-11-23 19:29:13 +0900

    공지사항 엑티비티 서버에서 데이터를 읽어와 존재한다면 어댑터를 사용하여 공지사항을 출력

---
+++ Yamyam/app/src/main/java/com/example/Yamyam/NoticeActivity.java
@@ -0,0 +1,164 @@
+package com.example.Yamyam;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NoticeActivity extends AppCompatActivity {
+
+    // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+    private RecyclerView mRVNotice;
+    private AdapterNotice mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_notice);
+
+        new AsyncFetch().execute();
+    }
+
+    // Create class AsyncFetch
+    private class AsyncFetch extends AsyncTask<String, String, String> {
+
+        ProgressDialog pdLoading = new ProgressDialog(NoticeActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+
+                // Enter URL address where your php file resides
+                url = new URL(AppConfig.URL_GPS_SEARCH);
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput to true as we send and recieve data
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+                    return("Connection error");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+            pdLoading.dismiss();
+            List<DataNotice> data=new ArrayList<>();
+
+            pdLoading.dismiss();
+            if(result.equals("no rows")) {
+                Toast.makeText(NoticeActivity.this, "검색 결과가 존재하지 않습니다.", Toast.LENGTH_LONG).show();
+            }else{
+
+                try {
+
+                    JSONArray jArray = new JSONArray(result);
+
+                    // Extract data from json and store into ArrayList as class objects
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject json_data = jArray.getJSONObject(i);
+                        DataNotice noticeData = new DataNotice();
+                        noticeData.noticeId = json_data.getString("serial");
+                        noticeData.noticeName = json_data.getString("name");
+                        noticeData.noticeDate = json_data.getString("date");
+
+                        data.add(noticeData);
+                    }
+
+                    // Setup and Handover data to recyclerview
+                    mRVNotice = (RecyclerView) findViewById(R.id.noticeList);
+                    mAdapter = new AdapterNotice(NoticeActivity.this, data);
+                    mRVNotice.setAdapter(mAdapter);
+                    mRVNotice.setLayoutManager(new LinearLayoutManager(NoticeActivity.this));
+
+                } catch (JSONException e) {
+                    // You to understand what actually error is and handle it appropriately
+                    Toast.makeText(NoticeActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(NoticeActivity.this, result.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
