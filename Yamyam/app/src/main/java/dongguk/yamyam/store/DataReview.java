75733b00fb5d91efaae38218a4c4e77e482eb6b6
sojeong <thwjddl94@naver.com>
2016-11-23 19:01:36 +0900

    리뷰의 정보들을 저장한 구조체 역할

---
+++ Yamyam/app/src/main/java/dongguk/yamyam/store/DataReview.java
@@ -0,0 +1,10 @@
+package dongguk.yamyam.store;
+
+/**
+ * Created by SJ on 2016-11-14.
+ */
+public class DataReview {
+    public String reviewId;
+    public String reviewDate;
+    public String reviewContent;
+}
