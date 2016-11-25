4cfb0372ced9aa5146eaad052b84c689600be55a
하나 <khn_0412@naver.com>
2016-11-25 15:06:53 +0900

    상세페이지에 탭을 추가

--- Yamyam/app/src/main/java/com/example/Yamyam/DetailActivity.java
+++ Yamyam/app/src/main/java/com/example/Yamyam/DetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,23 +16,19 @@ import com.kakao.util.KakaoParameterException;
 
 
 public class DetailActivity extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFFFA500));
-    }
-    public void kakao_onclick(View v){
-        try{
-            final KakaoLink kakaoLink=KakaoLink.getKakaoLink(this);
-            final KakaoTalkLinkMessageBuilder kakaoBuilder=kakaoLink.createKakaoTalkLinkMessageBuilder();
+        public void kakao_onclick(View v){
+            try{
+                final KakaoLink kakaoLink=KakaoLink.getKakaoLink(this);
+                final KakaoTalkLinkMessageBuilder kakaoBuilder=kakaoLink.createKakaoTalkLinkMessageBuilder();
 
-            //메세지 추가
-            kakaoBuilder.addText("카카오톡 공유");
+                //메세지 추가
+                kakaoBuilder.addText("카카오톡 공유");
 
-            //앱 실행 버튼
-            kakaoBuilder.addAppButton("냠냠이 실행");
+                //앱 실행 버튼
+                kakaoBuilder.addAppButton("냠냠이 실행");
 
             //메세지 발송
             kakaoLink.sendMessage(kakaoBuilder, this);
@@ -42,5 +40,49 @@ public class DetailActivity extends AppCompatActivity {
     public void call_onclick(View v){
         Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-1111-2222"));
         startActivity(intent);
+    }
+    public DetailActivity() {
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailpage);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFFFA500));
+
+        //탭 구현 코드
+        // Initializing the TabLayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout_detail);
+        tabLayout.addTab(tabLayout.newTab().setText("가게정보"));
+        tabLayout.addTab(tabLayout.newTab().setText("리뷰"));
+        tabLayout.addTab(tabLayout.newTab().setText("지도"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Initializing ViewPager
+        viewPager = (ViewPager) findViewById(R.id.pager_detail);
+
+        // Creating TabPagerAdapter adapter
+        final DetailTabAdapter pagerAdapter = new DetailTabAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        // Set TabSelectedListener
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                pagerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+        });
     }
 }
