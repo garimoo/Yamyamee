88b4253b6e1ebc6ec7ae4d041507d7ec8e319d4b
donggeon <dkkim04@naver.com>
2016-11-29 10:12:05 +0900

    찜하기 기능 수정(LikeActivity 사용하지 않음)

--- Yamyam/app/src/main/java/dongguk/yamyam/activity/DetailActivity.java
+++ Yamyam/app/src/main/java/dongguk/yamyam/activity/DetailActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
@@ -20,6 +21,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -27,13 +29,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
 import dongguk.yamyam.R;
 import dongguk.yamyam.app.AppConfig;
+import dongguk.yamyam.fragment.ReviewFragment;
 import dongguk.yamyam.helper.SQLiteHandler;
 import dongguk.yamyam.store.DataStore;
 
@@ -69,6 +75,14 @@ public class DetailActivity extends AppCompatActivity {
         favorite = (ImageButton) findViewById(R.id.likeButton);
 
         new AsyncFetch(id + "$" + key).execute();
+
+        favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // continueApp();
+                new AsyncFavorite(id + "$" + key).execute();
+            }
+        });
     }
 
     // Create class AsyncFetch
@@ -79,8 +93,8 @@ public class DetailActivity extends AppCompatActivity {
         URL url = null;
         String searchQuery;
 
-        public AsyncFetch(String searchQuery){
-            this.searchQuery=searchQuery;
+        public AsyncFetch(String searchQuery) {
+            this.searchQuery = searchQuery;
         }
 
         @Override
@@ -137,7 +151,6 @@ public class DetailActivity extends AppCompatActivity {
             }
 
             try {
-
                 int response_code = conn.getResponseCode();
 
                 // Check if successful connection made
@@ -157,7 +170,7 @@ public class DetailActivity extends AppCompatActivity {
                     return (result.toString());
 
                 } else {
-                    return("Connection error");
+                    return ("Connection error");
                 }
 
             } catch (IOException e) {
@@ -175,9 +188,9 @@ public class DetailActivity extends AppCompatActivity {
             pdLoading.dismiss();
 
             pdLoading.dismiss();
-            if(result.equals("no rows")) {
+            if (result.equals("no rows")) {
                 Toast.makeText(DetailActivity.this, "검색 결과가 존재하지 않습니다.", Toast.LENGTH_LONG).show();
-            }else{
+            } else {
 
                 try {
                     JSONArray jArray = new JSONArray(result);
@@ -195,10 +208,9 @@ public class DetailActivity extends AppCompatActivity {
                     storeData.storeLatX = json_data.getString("x_dnts");
                     storeData.storeLongY = json_data.getString("y_dnts");
 
-                    if(json_data.has("1") == true) {
+                    if (json_data.has("1") == true) {
                         favorite.setBackgroundResource(R.drawable.star1);
-                    }
-                    else favorite.setBackgroundResource(R.drawable.star2);
+                    } else favorite.setBackgroundResource(R.drawable.star2);
 
                     textName = (TextView) findViewById(R.id.textName);
                     textSubject = (TextView) findViewById(R.id.textSubject);
@@ -213,8 +225,8 @@ public class DetailActivity extends AppCompatActivity {
 
                     String stringScore = json_score.getString("AVG(score)");
 
-                    if(stringScore != "null") storeData.storeScore = Float.valueOf(stringScore);
-                    else storeData.storeScore = (float)0;
+                    if (stringScore != "null") storeData.storeScore = Float.valueOf(stringScore);
+                    else storeData.storeScore = (float) 0;
 
                     rb.setRating(storeData.storeScore);
                     textScore.setText(Float.toString(Math.round(storeData.storeScore * 100f) / 100f));
@@ -228,27 +240,124 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    public void like_onclick(View v){
-        Intent i=new Intent(this, LikeActivity.class);
-        i.putExtra("key", key); startActivity(i);
+    // Create class AsyncFetch
+    private class AsyncFavorite extends AsyncTask<String, String, String> {
+
+        ProgressDialog pdLoading = new ProgressDialog(DetailActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
+        String searchQuery;
+
+        public AsyncFavorite(String searchQuery) {
+            this.searchQuery = searchQuery;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                // Enter URL address where your php file resides
+                url = new URL(AppConfig.URL_FAVORITE);
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput to true as we send and recieve data
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // add parameter to our above url
+                Uri.Builder builder = new Uri.Builder().appendQueryParameter("searchQuery", searchQuery);
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+                    return ("Connection error");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //this method will be running on UI thread
+            pdLoading.dismiss();
+            if(result.equals("already exists")) {
+                Toast.makeText(DetailActivity.this, "즐겨찾기에서 제거되었습니다.", Toast.LENGTH_LONG).show();
+                favorite.setBackgroundResource(R.drawable.star2);
+            }
+
+            else if(result.equals("add successfully")) {
+                Toast.makeText(DetailActivity.this, "즐겨찾기에 추가되었습니다.", Toast.LENGTH_LONG).show();
+                favorite.setBackgroundResource(R.drawable.star1);
+            }
+        }
     }
 
     public void review_onclick(View v){
         Intent i=new Intent(this, ReviewListActivity.class);
-        i.putExtra("key", key); startActivity(i);
+        i.putExtra("key", key); startActivity(i); finish();
     }
 
     public void review_write_onclick(View v){
         Intent i=new Intent(this, WriteReviewActivity.class);
-        i.putExtra("key", key); startActivity(i);
+        ReviewFragment fragment = (ReviewFragment) f.findFragmentById(R.id.ReviewFragment);
+        // fragment.setData(key);
+
     }
 
     public void gps_onclick(View v){
         Intent i=new Intent(this, DetailGpsActivity.class);
         i.putExtra("xy_dnts", storeData.storeLatX +"," + storeData.storeLongY);
-        startActivity(i);
+        startActivity(i); finish();
     }
-
-
 }
-
